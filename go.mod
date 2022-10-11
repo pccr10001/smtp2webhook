72423a1306727fdae310bed856561c3004367716
Power Li <pccr10001@gmail.com>
2022-10-11 13:32:28 +0800

    change mail parser

--- go.mod
+++ go.mod
@@ -3,15 +3,28 @@ module github.com/pccr10001/smtp2webhook
 go 1.18
 
 require (
-	github.com/DusanKasan/parsemail v1.2.0
+	github.com/cention-sany/go.enmime v0.0.0-20201106071208-88a344d546c9
 	github.com/google/uuid v1.3.0
 	github.com/mhale/smtpd v0.8.0
-	golang.org/x/net v0.0.0-20221004154528-8021a29435af
 	gopkg.in/h2non/gentleman.v2 v2.0.5
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
+	github.com/cention-sany/mime v0.0.0-20190627104658-bc3a92ce4cf2 // indirect
+	github.com/cention-sany/net v0.0.0-20181002074506-63010f1b09ca // indirect
+	github.com/cention-sany/utf7 v0.0.0-20170124080048-26cad61bd60a // indirect
+	github.com/cention-sany/uuencode v0.0.0-20161209003816-261804667ccd // indirect
+	github.com/glycerine/goconvey v0.0.0-20190410193231-58a59202ab31 // indirect
+	github.com/glycerine/rbuf v0.0.0-20190314090850-75b78581bebe // indirect
+	github.com/gopherjs/gopherjs v1.17.2 // indirect
+	github.com/jaytaylor/html2text v0.0.0-20211105163654-bc68cce691ba // indirect
+	github.com/jtolds/gls v4.20.0+incompatible // indirect
+	github.com/kylelemons/godebug v1.1.0 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/nbio/st v0.0.0-20140626010706-e9e8d9816f32 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
+	github.com/ssor/bom v0.0.0-20170718123548-6386211fdfcf // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
 	golang.org/x/text v0.3.7 // indirect
 )
--- main.go
+++ main.go
@@ -2,16 +2,14 @@ package main
 
 import (
 	"bytes"
-	"github.com/DusanKasan/parsemail"
+	"github.com/cention-sany/go.enmime"
+	"github.com/cention-sany/net/mail"
 	"github.com/google/uuid"
 	"github.com/mhale/smtpd"
-	"golang.org/x/net/html/charset"
 	"gopkg.in/h2non/gentleman.v2"
 	"gopkg.in/yaml.v3"
-	"io"
 	"io/ioutil"
 	"log"
-	"mime"
 	"net"
 	"net/url"
 	"strings"
@@ -19,25 +17,10 @@ import (
 
 var accounts []Account
 
-func decode(text string) string {
-	CharsetReader := func(label string, input io.Reader) (io.Reader, error) {
-		label = strings.Replace(label, "windows-", "cp", -1)
-		encoding, _ := charset.Lookup(label)
-		return encoding.NewDecoder().Reader(input), nil
-	}
-	dec := mime.WordDecoder{CharsetReader: CharsetReader}
-
-	header, err := dec.DecodeHeader(text)
-
-	if err != nil {
-		return text
-	}
-	return header
-}
-
 func mailHandler(origin net.Addr, from string, to []string, data []byte) error {
-	msg, _ := parsemail.Parse(bytes.NewReader(data))
-	subject := decode(msg.Header.Get("Subject"))
+	msg, _ := mail.ReadMessage(bytes.NewReader(data))
+	mime, _ := enmime.ParseMIMEBody(msg)
+	subject := mime.GetHeader("Subject")
 
 	log.Printf("Received mail from %s for %s with subject %s", from, to[0], subject)
 
@@ -81,9 +64,9 @@ func mailHandler(origin net.Addr, from string, to []string, data []byte) error {
 	go gentleman.New().
 		Post().
 		URL(uri).
-		AddQuery("subject", url.QueryEscape(decode(subject))).
+		AddQuery("subject", url.QueryEscape(subject)).
 		AddHeader("Content-Type", "text/plain").
-		BodyString(msg.HTMLBody).
+		BodyString(mime.HTML).
 		Do()
 
 	log.Printf("Webhook emitted, %s %s", account.Webhook.Host, account.Webhook.Id)
